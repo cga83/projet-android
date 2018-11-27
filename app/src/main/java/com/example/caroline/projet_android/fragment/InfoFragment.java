7cee8dca8badac5ed4c6ec2225ead2fcfeae68fc
Caroline Galliez <caroline@galliez.com>
2018-11-27 13:06:58 +0100

    Informations sur les lieux de tournages

--- app/src/main/java/com/example/caroline/projet_android/fragment/InfoFragment.java
+++ app/src/main/java/com/example/caroline/projet_android/fragment/InfoFragment.java
@@ -15,21 +15,34 @@ import com.example.caroline.projet_android.R;
 public class InfoFragment extends Fragment {
 
     private InfoFragmentInterface mListener;
-    private TextView text;
+    private TextView textTitle;
+    private TextView textInfo;
 
     public InfoFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
-        text = rootView.findViewById(R.id.info);
+        View rootView = inflater.inflate(R.layout.fragment_info, container, false);
+        textTitle = rootView.findViewById(R.id.info_title);
+        textTitle.setText("Informations sur les données de l'application");
 
-        text.setText("tettt");
+        textInfo = rootView.findViewById(R.id.info_text);
+        textInfo.setText("Les données utilisées sont celles de lieux de tournages à Paris.\n\n" +
+                "Elles ont été récupérées sur le site https://opendata.paris.fr/explore/dataset/tournagesdefilmsparis2011/table/. \n\n" +
+                "Pour chaque lieu de tournage, nous avons les informations suivantes :\n\n" +
+                "- Le titre du film\n" +
+                "- Le réalisateur du film\n" +
+                "- L'adresse du lieu de tournage\n" +
+                "- L'organisme demandeur du film\n" +
+                "- Le type du tournage (long métrage, téléfilm ou série télévisée)\n" +
+                "- L'arondissement dans lequel le tournage a eu lieu\n" +
+                "- La date de début du tournage\n" +
+                "- La date de fin du tournage\n" +
+                "- Les coordonnées du lieu de tournage");
         return rootView;
     }
 
